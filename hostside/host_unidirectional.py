a4ad88c7486a3b7c9cdee29edbde76586b940e23
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-04-18 11:43:14 +0800

    host_unidirectional.py for Darwin

--- hostside/host_unidirectional.py
+++ hostside/host_unidirectional.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 import serial
 import sys
+import platform
+import glob
 
-dev = serial.Serial("/dev/ttyUSB0", 115200)
+if platform.system() == "Darwin":
+    devices = glob.glob("/dev/tty.usbserial*")
+    if not devices:
+        print("No tty.usbserial devices found.")
+        sys.exit(1)
+    dev = serial.Serial(devices[0], 38400)
+else:
+    dev = serial.Serial("/dev/ttyUSB0", 38400)
 
 print("> Returned data:", file=sys.stderr)
 
